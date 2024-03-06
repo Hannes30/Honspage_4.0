a367ad0904ccf0ca7757e15f8133eb61756f4fcd
Hannes30 <hannes.scheibelauer@sz-ybbs.ac.at>
2024-03-06 14:38:11 +0100

    feat: pricing concept

---
+++ src/lib/components/WebDevPricing.svelte
@@ -0,0 +1,69 @@
+<script>
+    import {CheckOutline} from "flowbite-svelte-icons";
+
+const pricingModels = [
+  {
+    name: "Web-Starter",
+    price: 700,
+    benefits: [
+      "3-5 Seiten",
+      "Responsives Layout",
+      "Grundlegende SEO-Optimierung"
+    ]
+  },
+  {
+    name: "Business Website Plus",
+    price: 1400,
+    benefits: [
+      "5-7 Seiten mit professionellem, anpassbarem Design",
+      "Bildergalerie oder Portfolio-Sektion",
+      "Erweitertes Kontaktformular",
+      "Grundlegende SEO-Optimierung",
+      "Google Maps-Integration"
+    ]
+  },
+  {
+    name: "E-Commerce Pro",
+    price: 2450,
+    benefits: [
+      "E-Commerce-Funktionalität mit Produktdarstellung, Warenkorb und Checkout",
+      "Zahlungsgateway-Integration (z.B., PayPal, Stripe)",
+      "Produktbilder und -beschreibungen",
+      "Kundenkonto und einfache Bestellverfolgung"
+    ]
+  },
+  {
+    name: "Maßgeschneiderte Unternehmenswebsite Deluxe",
+    price: 4000,
+    benefits: [
+      "Umfassende Website mit individuellem Design",
+      "Fortgeschrittene SEO-Strategie und -Implementierung",
+      "Benutzerdefinierte Funktionen und Programmierung",
+      "Schulung und Support für die Website-Verwaltung",
+      "Erweiterte Sicherheitsfunktionen"
+    ]
+  }
+];
+</script>
+
+<div class="w-full text-center">  <h2>Pricing</h2>
+    <div class="w-full flex justify-around">
+
+        <div class="border border-primary-400 rounded-md p-4 flex justify-center flex-wrap w-96 bg-secondary">
+            <h2 class=" text-4xl w-full text-center py-2">{pricingModels[0].name}</h2>
+            <span class="w-full text-center text-4xl font-bold text-primary-400 py-2">€{pricingModels[0].price}</span>
+            <div class="text-center flex flex-wrap py-2">
+            {#each pricingModels[0].benefits as  benefit}
+            <div class="w-full justify-around"> <CheckOutline class=" w-12 h-12 inline-block"/>  <span class=" text-lg">  {benefit} </span></div>
+               
+            {/each}</div>   
+            <button class="px-6 py-2 font-semibold bg-primary-400 rounded-md button text-black text-3xl"> Kontaktieren</button>
+        </div>
+
+    </div>
+
+
+</div>
+<style>.button{
+    text-shadow: 2px 2px #fff;
+  }</style>
--- src/routes/services/[service]/+page.svelte
+++ src/routes/services/[service]/+page.svelte
@@ -2,6 +2,7 @@
     import Nav from "$lib/components/Nav.svelte";
   import ProjectTag from "$lib/components/ProjectTag.svelte";
   import Waves from "$lib/components/Waves.svelte";
+  import WebDevPricing from "../../../lib/components/WebDevPricing.svelte";
     export let data;
 </script>
 <svelte:head>
@@ -10,4 +11,6 @@
 <Nav active=3></Nav>
 <div class="pt-16 flex justify-center flex-wrap">
         <h1 class="w-full text-center">{data.name}</h1>
+        <span class=" text-center lg:text-3xl text-3xl p-16">{data.text}</span>
+        <WebDevPricing></WebDevPricing>
 </div>
