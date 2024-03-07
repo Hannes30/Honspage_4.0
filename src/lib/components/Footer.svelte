fc46d79baca29789dc8d33aad63a5ea4bd3e2c2f
Hannes0_0 <hannes.scheibelauer@sz-ybbs.ac.at>
2024-03-07 18:15:48 +0100

    feat: seo optimaziation

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -20,14 +20,14 @@
 <footer class="mt-20 bg-primary-400 text-secondary p-5">
     <div class="text-2xl font-medium text-center sm:text-3xl">© Hannes Scheibelauer</div>
     <div class="flex justify-around">
-        <div class="text-l sm:leading-loose sm:text-xl leading-7"> <div class="text-2xl sm:text-3xl font-medium leading-tight"> Contact </div>Hannes Scheibelauer <p>kontakt@hannes-scheibelauer.at
-            </p> <p>0670 2018717</p>
+        <div class="text-l sm:leading-loose sm:text-xl leading-7"> <div class="text-3xl font-medium leading-tight"> Contact </div><p class="py-2">Hannes Scheibelauer</p> <p class="py-2">kontakt@hannes-scheibelauer.at
+            </p> <p class="py-2">0670 2018717</p>
         </div>
-        <div class="text-l sm:leading-loose sm:text-xl leading-7"> <div class="text-2xl sm:text-3xl font-medium leading-tight p-1"> Pages </div>
-            <p><a href="/">Home</a></p>
-            <p><a href="/projects">Projekte</a></p>
-            <p><a href="/services">Leistungen</a></p>
-            <p><a href="/impressum">Impressum</a></p>
+        <div class="text-l sm:leading-loose sm:text-xl leading-7"> <div class="text-3xl font-medium leading-tight p-1"> Pages </div>
+            <p class="text-2xl opacity-80 py-2"><a href="/">Home</a></p>
+            <p class="text-2xl opacity-80 py-2"><a href="/projects">Projekte</a></p>
+            <p class="text-2xl opacity-80 py-2"><a href="/services">Leistungen</a></p>
+            <p class="text-2xl opacity-80 py-2"><a href="/impressum">Impressum</a></p>
         </div>
     </div>
 </footer>
