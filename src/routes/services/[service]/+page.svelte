3289de49d1a6549cce0be13e32c2b5f772fcc7f6
Hannes0_0 <hannes.scheibelauer@sz-ybbs.ac.at>
2024-03-07 19:18:54 +0100

    fix: responsive issues fixed

--- src/routes/services/[service]/+page.svelte
+++ src/routes/services/[service]/+page.svelte
@@ -12,7 +12,7 @@
 <Nav active=3></Nav>
 <div class="pt-16 flex justify-center flex-wrap">
         <h1 class="w-full text-center">{data.services.name}</h1>
-        <span class=" text-center lg:text-3xl text-3xl w-3/4 py-16">{data.services.text}</span>
+        <span class=" text-center lg:text-3xl  w-3/4 py-16 text-lg">{data.services.text}</span>
         {#if data.services.name =="Web Development"}
          <WebDevPricing></WebDevPricing>
          {:else if data.services.name== "Web Hosting"}
