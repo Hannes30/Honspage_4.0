501546857654428b3628d1c6ac55a7535d4763de
Hannes30 <hannes.scheibelauer@sz-ybbs.ac.at>
2024-03-11 11:45:35 +0100

    feat: added Portfolio Project

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -2,6 +2,7 @@
   import Nav from "$lib/components/Nav.svelte";
   import Banner from "$lib/components/Banner.svelte";
   import HofcafeImage from "$lib/img/projects/Hofcafe_Collage.jpg"
+  import PortfolioImage from "$lib/img/projects/Portfolio_Collage.jpg"
 </script>
 <svelte:head>
 	<title>Projekte - Hannes Scheibelauer</title>
@@ -9,5 +10,6 @@
 <Nav active=2></Nav>
 <div class="pt-20 flex justify-center flex-wrap">
   <h1 class="w-full text-center">Projekte</h1>
-  <Banner image={HofcafeImage} name="Hofcafe Wolfpassing" text="August 2023 - Januar 2024" route="projects"></Banner>
+  <Banner image={HofcafeImage} name="Hofcafe Wolfpassing" text="August 2023" route="projects"></Banner>
+  <Banner image={PortfolioImage} name="Portfolio Website" text="Februar 2024 - März 2024" route="projects"></Banner>
 </div>
