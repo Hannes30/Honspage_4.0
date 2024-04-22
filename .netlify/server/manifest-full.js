709fc351638d2001bcfd16f9568cc55c0c9cb5f8
Hannes30 <hannes.scheibelauer@sz-ybbs.ac.at>
2024-04-22 10:08:24 +0200

    fix: removed .nojekelly

--- .netlify/server/manifest-full.js
+++ .netlify/server/manifest-full.js
@@ -7,10 +7,10 @@ function __memo(fn) {
 return {
 	appDir: "_app",
 	appPath: "_app",
-	assets: new Set([".nojekyll","favicon.png","favicon2.png","Logo.png"]),
+	assets: new Set(["favicon.png","favicon2.png","Logo.png"]),
 	mimeTypes: {".png":"image/png"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.u1oGe74a.js","app":"_app/immutable/entry/app.hXGIBRf3.js","imports":["_app/immutable/entry/start.u1oGe74a.js","_app/immutable/chunks/scheduler.9rETXCuu.js","_app/immutable/chunks/index.SNfDup5C.js","_app/immutable/entry/app.hXGIBRf3.js","_app/immutable/chunks/preload-helper.0HuHagjb.js","_app/immutable/chunks/scheduler.9rETXCuu.js","_app/immutable/chunks/index.GzZsvtwO.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
+		client: {"start":"_app/immutable/entry/start.mdZouQRw.js","app":"_app/immutable/entry/app.hXGIBRf3.js","imports":["_app/immutable/entry/start.mdZouQRw.js","_app/immutable/chunks/scheduler.9rETXCuu.js","_app/immutable/chunks/index.SNfDup5C.js","_app/immutable/entry/app.hXGIBRf3.js","_app/immutable/chunks/preload-helper.0HuHagjb.js","_app/immutable/chunks/scheduler.9rETXCuu.js","_app/immutable/chunks/index.GzZsvtwO.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
