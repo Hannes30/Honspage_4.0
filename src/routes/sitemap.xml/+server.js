3d70c6e17a5728eeb6943544a93f0b80f9d3e748
Hannes30 <hannes.scheibelauer@sz-ybbs.ac.at>
2024-04-22 09:09:12 +0200

    itemap.xml changes

--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -1,8 +1,6 @@
 export async function GET() {
   return new Response(
     `		<?xml version="1.0" encoding="UTF-8" ?>		<urlset			xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"			xmlns:xhtml="https://www.w3.org/1999/xhtml"			xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"			xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"			xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"			xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"		>	
-    <?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
         <loc>http://hannes-scheibelauer.at/</loc>
         <lastmod>2024-04-22</lastmod>
@@ -27,7 +25,6 @@ export async function GET() {
         <changefreq>yearly</changefreq>
         <priority>0.7</priority>
     </url>
-</urlset>
 
             </urlset>`.trim(),
     { headers: { "Content-Type": "application/xml" } }
