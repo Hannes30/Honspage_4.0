078aaa2686caa86c039516faab5e9f772c343682
Hannes30 <hannes.scheibelauer@sz-ybbs.ac.at>
2024-04-26 16:02:37 +0200

    fix: neues Impressum

--- src/routes/impressum/+page.svelte
+++ src/routes/impressum/+page.svelte
@@ -6,27 +6,26 @@
 	<title>Impressum - Hannes Scheibelauer</title>
 </svelte:head>
 <Nav active=3></Nav>
+<div class="pt-12"></div>
 <h1>Impressum</h1>
-        
-<h2>Angaben gemäß § 5 TMG:</h2>
-<p>Name: Hannes Scheibelauer</p>
-<p>Adresse: Bahnstraße 64, 3250 Mühling</p>
-<p>Telefon: 0670 2018717</p>
-<p>E-Mail: kontakt@hannes-scheibelauer.at</p>
 
-<h2>Umsatzsteuer-ID:</h2>
-<p>15 313/5447</p>
+<p>Hannes Scheibelauer<br />
+Web- &amp; Mediendesign<br />
+Bahnstra&szlig;e 64<br />
+3250 M&uuml;hling</p>
 
+<h2>Kontakt</h2>
+<p>Telefon: 0670 2018717<br />
+E-Mail: kontakt@hannes-scheibelauer.at</p>
 
-<h2>Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV:</h2>
-<p>Hannes Scheibelauer</p>
-<p>Bahnstraße 64, 3250 Mühling</p>
+<h2>Umsatzsteuer-ID</h2>
+<p>Umsatzsteuer-Identifikationsnummer gem&auml;&szlig; &sect; 27 a Umsatzsteuergesetz:<br />
+15 313/5447</p>
 
-<h2>Haftungsausschluss (Disclaimer):</h2>
-<p>Die Inhalte dieser Website werden mit größtmöglicher Sorgfalt erstellt. Der Anbieter übernimmt jedoch keine Gewähr für die Richtigkeit, Vollständigkeit und Aktualität der bereitgestellten Inhalte. Die Nutzung der Inhalte der Website erfolgt auf eigene Gefahr des Nutzers.</p>
-<p>Haftungsansprüche gegen den Anbieter, welche sich auf Schäden materieller oder ideeller Art beziehen, die durch die Nutzung oder Nichtnutzung der dargebotenen Inhalte bzw. durch die Nutzung fehlerhafter und unvollständiger Inhalte verursacht wurden, sind grundsätzlich ausgeschlossen, sofern seitens des Anbieters kein nachweislich vorsätzliches oder grob fahrlässiges Verschulden vorliegt.</p>
-<p>Alle Angebote sind freibleibend und unverbindlich. Der Anbieter behält es sich ausdrücklich vor, Teile der Seiten oder das gesamte Angebot ohne gesonderte Ankündigung zu verändern, zu ergänzen, zu löschen oder die Veröffentlichung zeitweise oder endgültig einzustellen.</p>
-<p>Bei direkten oder indirekten Verweisen auf fremde Webseiten ("Hyperlinks"), die außerhalb des Verantwortungsbereiches des Anbieters liegen, würde eine Haftungsverpflichtung ausschließlich in dem Fall in Kraft treten, in dem der Anbieter von den Inhalten Kenntnis hat und es ihm technisch möglich und zumutbar wäre, die Nutzung im Falle rechtswidriger Inhalte zu verhindern.</p>
-<p>Der Anbieter erklärt hiermit ausdrücklich, dass zum Zeitpunkt der Linksetzung keine illegalen Inhalte auf den zu verlinkenden Seiten erkennbar waren. Auf die aktuelle und zukünftige Gestaltung, die Inhalte oder die Urheberschaft der verlinkten/verknüpften Seiten hat der Anbieter keinerlei Einfluss. Deshalb distanziert er sich hiermit ausdrücklich von allen Inhalten aller verlinkten /verknüpften Seiten, die nach der Linksetzung verändert wurden.</p>
-<p>Diese Feststellung gilt für alle innerhalb des eigenen Internetangebotes gesetzten Links und Verweise sowie für Fremdeinträge in vom Anbieter eingerichteten Gästebüchern, Diskussionsforen und Mailinglisten. Für illegale, fehlerhafte oder unvollständige Inhalte und insbesondere für Schäden, die aus der Nutzung oder Nichtnutzung solcherart dargebotener Informationen entstehen, haftet allein der Anbieter der Seite, auf welche verwiesen wurde, nicht derjenige, der über Links auf die jeweilige Veröffentlichung lediglich verweist.</p>
-<p>Alle Texte, Bilder, Grafiken, Ton-, Video- und Animationsdateien dieser Website unterliegen dem Urheberrecht und anderen Gesetzen zum Schutz geistigen Eigentums. Eine Weiterverwendung, auch auszugsweise, ist ohne schriftliche Genehmigung des Urhebers nicht gestattet.</p>
+<h2>EU-Streitschlichtung</h2>
+<p>Die Europ&auml;ische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit: <a href="https://ec.europa.eu/consumers/odr/" target="_blank" rel="noopener noreferrer">https://ec.europa.eu/consumers/odr/</a>.<br /> Unsere E-Mail-Adresse finden Sie oben im Impressum.</p>
+
+<h2>Verbraucher&shy;streit&shy;beilegung/Universal&shy;schlichtungs&shy;stelle</h2>
+<p>Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.</p>
+
+<p>Quelle: <a href="https://www.e-recht24.de/impressum-generator.html">https://www.e-recht24.de/impressum-generator.html</a></p>
